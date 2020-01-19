df2a761df755e9c56446a3638bf10d5262641084
hwan71 <hwan71.dp@gmail.com>
2020-01-19 21:52:14 +0900

    산 술 연 산 자

---
+++ chapter03/src/sec02/exam04/ArithmeticOperatorExample.java
@@ -0,0 +1,36 @@
+package sec02.exam04;
+
+public class ArithmeticOperatorExample {
+
+	public static void main(String[] args) {
+			
+		System.out.println("********* 산 술 연 산 자 **********");
+		
+			int v1 = 5;
+			int v2 = 2;
+			
+			int result1 = v1 + v2;
+			System.out.println("\tresult1: " + result1);
+			
+			int result2 = v1 - v2;
+			System.out.println("\tresult2: " + result2);
+			
+			int result3 = v1 * v2;
+			System.out.println("\tresult3: " + result3);
+			
+			int result4 = v1 / v2;
+			System.out.println("\tresult4: " + result4);
+			
+			double result5 = v1 / v2;
+			System.out.println("\tresult5: " + result5);
+			
+			int result6 = v1 % v2;
+			System.out.println("\tresult6: " + result6);
+			
+		System.out.println("****************************");	
+			
+			
+
+	}
+
+}
