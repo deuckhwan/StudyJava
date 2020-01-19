9381beecb0618d86e040aff400f379037b97e30e
hwan71 <hwan71.dp@gmail.com>
2020-01-19 21:57:29 +0900

    String 문자열 결합 연산자

---
+++ chapter03/src/sec02/exam06/StringConcatExample.java
@@ -0,0 +1,19 @@
+package sec02.exam06;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("******* 문 자 열 결 합 연 산 자 *********");
+		
+		String str1 = "JDK" + 6.0;
+		String str2 = str1 + "특징";
+		
+		System.out.println("\t" + str2);
+		
+		System.out.println("*******************************");
+		
+
+	}
+
+}
